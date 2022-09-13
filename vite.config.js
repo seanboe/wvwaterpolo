a2b122feff91b3549ae39e389c81e270b0d73675
Sean Boerhout <boeseany@gmail.com>
2022-09-13 16:38:55 -0700

    udpates

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        coaches: resolve(__dirname, '/coaches.html'),
+        roster: resolve(__dirname, '/roster.html'),
+        schedule: resolve(__dirname, '/schedule.html')
+      }
+    }
+  }
+})
